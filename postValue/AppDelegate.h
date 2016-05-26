3d3b95374528e68032dafb6a8d948f0ff55d8ca0
刘清灿 <lqc_loadingu@163.com>
2016-05-26 14:55:48 +0800

    Initial Commit

---
+++ postValue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  postValue
+//
+//  Created by weixvn on 16/5/26.
+//  Copyright © 2016年 weixvn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ postValue/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  postValue
+//
+//  Created by weixvn on 16/5/26.
+//  Copyright © 2016年 weixvn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
